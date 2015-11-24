938cc8c7bcd5c608a30a8920d7c2df4d636d8d63
misahw <Misah@airbears2-10-142-142-66.airbears2.1918.berkeley.edu>
2015-11-23 17:09:16 -0800

    fixed scrolling issues

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	$(".nav-button").click(function(){
 		if ($(".nav-button").hasClass("nav-button-active")){
-			$(".nav").hide();
-			$(".content").addClass("nav-closed");
+			$(".wrapper").addClass("nav-closed");
 			$(".nav-button").removeClass("nav-button-active");
+			$(".nav").removeClass("nav-active");
 		} else{
 			$(".nav-button").addClass("nav-button-active");
-			$(".nav").show();
-			$(".content").removeClass("nav-closed");
+			$(".nav").addClass("nav-active");
+			$(".wrapper").removeClass("nav-closed");
 		}
 	});
 
